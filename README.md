3647fc2b4b391a07196b8fc8f7da81b6864b8f65
Ogi <shnchrog@gmail.com>
2021-05-13 00:31:37 +0900

    changed add docker environment

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # face-recognition
+
+## testing environments using docker on local.
+
+```bash
+. docker/build.sh
+```
+
+http://localhost:5000/
---
+++ docker/build.sh
@@ -0,0 +1,3 @@
+docker build -t face-recog -f docker/Dockerfile .
+docker run --rm -d -p 5000:5000 face-recog:latest
+# Access http://localhost:5000/ on your browser. 
