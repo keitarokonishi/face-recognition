3b362c99f0327950ac6ed7df531105dc2b8f1828
Keitaro <47139885+keitarokonishi@users.noreply.github.com>
2021-05-12 21:19:03 +0900

    Update server.py

--- server.py
+++ server.py
@@ -61,4 +61,5 @@ def upload():
 if __name__ == '__main__':
     app.debug = True
 #     app.run(host='0.0.0.0', port=8888)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
