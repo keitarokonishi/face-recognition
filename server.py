72ded814f797e906606b2e07c3eba7dc32ad8194
muripoLife <life.is.too.high.difficulty@gmail.com>
2021-05-15 23:32:06 +0900

    changd add logger

--- server.py
+++ server.py
@@ -83,6 +83,6 @@ if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
 
-    # logger.create_logfile()
+    logger.create_logfile()
 
     app.run(host='0.0.0.0', port=port)
